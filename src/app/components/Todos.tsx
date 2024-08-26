1bdefe66ec8b4a61c862d540bbcdeec23d0796c1
Brandon Fajardo <brandonjfajardo@hotmail.com>
2024-08-26 06:01:58 -0700

    fix: add spinner and prettier

--- src/app/components/Todos.tsx
+++ src/app/components/Todos.tsx
@@ -1,29 +1,67 @@
 "use client";
-import { useState } from "react"
+import { useEffect, useState } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Todos = () => {
-    const [inputValue, setInputValue] = useState<string>()
-    const [todos, setTodos] = useState<any>([])
+  const [inputValue, setInputValue] = useState<string>();
+  const [todos, setTodos] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const submit = () => {
-        if (!inputValue) return
+  useEffect(() => {
+    const getTodos = async () => {
+      try {
+        const response = await fetch("http://localhost:8080/todos");
 
-        setTodos([...todos, {
-            text: inputValue,
-            completed: false
-        }])
-        setInputValue('')
-    }
-    return (
-        <div>
-            <input type='text' value={inputValue} onChange={e => setInputValue(e.target.value)} />
-            <button onClick={submit}>Submit</button>
-            <br />
-            {todos.map((todo: any) => {
-                return <p key={todo.text}>{todo.text}</p>
-            })}
-        </div>
-    )
-}
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const todos = await response.json();
+        setTodos(todos);
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.error("Error fetching todos:", error);
+      }
+    };
 
-export default Todos
+    setTimeout(() => {
+      getTodos();
+    }, 1000);
+  }, []);
+
+  const submit = async () => {
+    if (!inputValue) return;
+
+    setTodos([
+      ...todos,
+      {
+        text: inputValue,
+        completed: false,
+      },
+    ]);
+    setInputValue("");
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button onClick={submit}>Submit</button>
+      <br />
+
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          {todos.map((todo: any) => {
+            return <p key={todo.text}>{todo.text}</p>;
+          })}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Todos;
